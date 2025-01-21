5bca0582291d2316c7afaeca337aaf05cc65f69a
Beverly <chaoscgo@hotmail.com>
2025-01-21 13:54:26 -0600

    Exercise 2 completed

--- exercise.py
+++ exercise.py
@@ -38,6 +38,7 @@ print_greeting()
 # - Ensure to provide feedback for non-alphabetical or invalid entries.
 
 def check_letter():
+    # Your control flow logic goes here
     vowel = ['a', 'e', 'i', 'o', 'u']
     letter = input('Enter one upper or lowercase letter:')
     print(type(letter))
@@ -54,3 +55,38 @@ def check_letter():
 
 # Call the function
 check_letter()
+
+
+# Exercise 2: Old enough to vote?
+#
+# Write a Python function named `check_voting_eligibility` that determines if a user is old enough to vote.
+# Fill in the logic to perform the eligibility check inside the function.
+#
+# Function Details:
+# - Prompt the user to input their age: "Please enter your age: "
+# - Validate the input to ensure the age is a possible value (no negative numbers).
+# - Determine if the user is eligible to vote. Set a variable for the voting age.
+# - Print a message indicating whether the user is eligible to vote based on the entered age.
+#
+# Hints:
+# - Use the `input()` function to capture the user's age.
+# - Use `int()` to convert the input to an integer. Ensure to handle any conversion errors gracefully.
+# - Use a conditional statement to check if the age meets the minimum voting age requirement.
+
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    voting_age = 18
+    age = input('Please enter your age:')
+    if age.isnumeric():
+        actual_age = int(age)
+        if actual_age >= voting_age:
+            print(f'Since you are {actual_age}, you are eligible to vote!')
+        else:
+            print(f'Since you are only {actual_age}, you are not yet eligible to vote')
+    else:
+        print('You must enter an actual age, please try again')
+
+
+
+# Call the function
+check_voting_eligibility()
